19fb1a24051c7629e0d8f67231f74eba920ee6a1
Noura <546590@student.saxion.nl>
2024-12-10 21:57:04 +0100

    created testing enviroment

--- BasicGame/src/codeScene1/BasicGame.java
+++ BasicGame/src/codeScene1/BasicGame.java
@@ -8,7 +8,7 @@ import nl.saxion.app.interaction.MouseEvent;
 public class BasicGame implements GameLoop {
 
     public static void main(String[] args) {
-        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
+        SaxionApp.startGameLoop(new BasicGame(), 1300, 800, 40);
     }
 
     @Override
@@ -19,6 +19,9 @@ public class BasicGame implements GameLoop {
     @Override
     public void loop() {
         SaxionApp.clear();
+        SaxionApp.drawImage("BasicGame/resources/background scene1.png",0, 0, 1300, 800);
+        SaxionApp.drawImage("BasicGame/resources/BISH.png",50, 440, 350, 300);
+        SaxionApp.drawImage("BasicGame/resources/Can.png",600, 580, 100, 100);
 
     }
 
@@ -29,7 +32,22 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
+        int mouseX = mouseEvent.getX();
+        int mouseY = mouseEvent.getY();
 
+        System.out.println("Clicked at (" + mouseX + ", " + mouseY + ")");
+
+        int canX = 600;
+        int canY = 580;
+        int canWidth = 100;
+        int canHeight = 100;
+
+        if (mouseEvent.isMouseDown()) {
+            if (mouseX >= canX && mouseX <= canX + canWidth &&
+                    mouseY >= canY && mouseY <= canY + canHeight) {
+                System.out.println("Clicked on the can!");
+            }
+        }
     }
 }
 
