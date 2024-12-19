be29d1587ac9033fa19439ae4020ae6e1e90dd5b
werckmann <497874@student.saxion.nl>
2024-12-19 14:20:45 +0100

    fixed errors related corrupted file in inventory system experimentation

--- BasicGame/src/TestingPuzzleSystem/BasicGame2.java
+++ BasicGame/src/TestingPuzzleSystem/BasicGame2.java
@@ -5,21 +5,18 @@ import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
-public class BasicGame implements GameLoop {
+public class BasicGame2 implements GameLoop {
 
     public static void main(String[] args) {
-        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
+        SaxionApp.startGameLoop(new BasicGame2(), 1000, 1000, 40);
     }
 
     @Override
     public void init() {
         boolean[] inventory = new boolean[4];
-            for (int i=0; i < inventory.length; i++) {
-                inventory[i] = false;
-            }
+        for (int i = 0; i < 4; i++) {
+            System.out.println("inventory[" + i + "] = " + inventory[i]);
         }
-
-
     }
 
     @Override
