abdb25353e127f9422ee9ce805a5327586a9fa9e
werckmann <497874@student.saxion.nl>
2025-01-14 18:12:20 +0100

    basic per scene interaction

--- BasicGame/src/basicGameloop/inSceneMouseEvents.java
+++ BasicGame/src/basicGameloop/inSceneMouseEvents.java
@@ -5,36 +5,36 @@ import nl.saxion.app.interaction.MouseEvent;
 public class inSceneMouseEvents {
 
     public static void click(MouseEvent mouseEvent, int sceneCounter) {
-//        if(mouseEvent.isMouseDown()){
-//        switch (sceneCounter) {
-//            case 0:
-//                if(mouseEvent.getX()>=600 && mouseEvent.getX() <=736 && mouseEvent.getY() >=580 && mouseEvent.getY()<=706 ){
-//
-//                }
-//                break;
-//            case 1:
-//                if(mouseEvent.getX()>= && mouseEvent.getX() <= && mouseEvent.getY() >= && mouseEvent.getY() <= ){
-//
-//                }
-//                break;
-//            case 2:
-//                if(mouseEvent.getX()>= && mouseEvent.getX() <= && mouseEvent.getY() >= && mouseEvent.getY() <= ){
-//
-//                }
-//                break;
-//            case 3:
-//                if(mouseEvent.getX()>= && mouseEvent.getX() <= && mouseEvent.getY() >= && mouseEvent.getY() <= ){
-//
-//                }
-//                break;
-//            case 4:
-//                if(mouseEvent.getX()>= && mouseEvent.getX() <= && mouseEvent.getY() >= && mouseEvent.getY() <= ){
-//
-//                }
-//                break;
-//            default:
-//                break;
-//        }}
+        if(mouseEvent.isMouseDown()){
+        switch (sceneCounter) {
+            case 0:
+                if(mouseEvent.getX()>=600 && mouseEvent.getX() <=736 && mouseEvent.getY() >=580 && mouseEvent.getY()<=706 ){
+                    System.out.println("0hit");
+                }
+                break;
+            case 1:
+                if(mouseEvent.getX()>=755 && mouseEvent.getX() <=955 && mouseEvent.getY() >=485 && mouseEvent.getY() <=685 ){
+                System.out.println("1hit");
+                }
+                break;
+            case 2:
+                if(mouseEvent.getX()>=-50 && mouseEvent.getX() <=550 && mouseEvent.getY() >=100 && mouseEvent.getY() <=700 ){
+                    System.out.println("2hit");
+                }
+                break;
+            case 3:
+                if(mouseEvent.getX()>=276 && mouseEvent.getX() <=876 && mouseEvent.getY() >=558 && mouseEvent.getY() <=708 ){
+                    System.out.println("3hit");
+                }
+                break;
+            case 4:
+                if(mouseEvent.getX()>=600 && mouseEvent.getX() <=800 && mouseEvent.getY() >=580 && mouseEvent.getY() <=780 ){
+                    System.out.println("4hit");
+                }
+                break;
+            default:
+                break;
+        }}
     }
 
 
